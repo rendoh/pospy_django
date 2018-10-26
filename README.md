72c7a993fe74eebc5cc4cfc523dcb9299dd0943a
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-10-26 18:18:17 +0900

    Migrate

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 ```
 $ pip install -r requirements.txt
 ```
+
+```
+django-admin startproject PROJECT
+python manage.py startapp APP_NAME
+```
+
+add to `INSTALLED_APPS`
+
+```
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+```
--- post/models.py
+++ post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=128)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_followers(self):
+        relations = Relationship.objects.filter(owner=self)
+        return [relation.follower for relation in relations]
+
+    def get_users_following_me(self):
+        relations = Relationship.objects.filter(follower=self)
+        return [relation.owner for relation in relations]
+
+class Relationship(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owners')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
