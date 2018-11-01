904f91b659e2780df0affd64ad973693929077b4
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-11-01 12:07:13 +0900

    Update

--- pospy/settings.py
+++ pospy/settings.py
@@ -144,3 +144,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
