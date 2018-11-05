df98ed2aaa2d901dd2f213b5c6e8cc205c42c1b3
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-11-05 16:46:47 +0900

    Update admin to add inline avatar field

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, UserAvatar, Relationship
 
-admin.site.register(User, UserAdmin)
+class AvatarInline(admin.StackedInline):
+    model = UserAvatar
+
+class MyUserAdmin(UserAdmin):
+    inlines = (AvatarInline,)
+
+admin.site.register(User, MyUserAdmin)
 admin.site.register(UserAvatar)
 admin.site.register(Relationship)
