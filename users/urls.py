8a3398d1e927c2d9abb84ce024f0b06116ca77e0
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-11-06 11:59:29 +0900

    Update urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,10 @@
 from rest_framework import routers
 from .views import UserViewSet, UserAvatarView
-from django.conf.urls import url
+from django.urls import path
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    url(r'^users/avatar/$', UserAvatarView.as_view()),
+    path('users/avatar/', UserAvatarView.as_view()),
 ]
