c9d1a5dc6ec5842ce3848b959753bb40cff06e86
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-11-07 12:09:50 +0900

    Update image deletion

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,12 @@
+import urllib.parse
 from django.db import models
-from users.models import User
+from django.core.validators import FileExtensionValidator
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import Q
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFit
-from django.core.validators import FileExtensionValidator
+from users.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -30,3 +34,28 @@ class PostImage(models.Model):
         processors=[ResizeToFit(980, upscale=False)],
         format='JPEG'
     )
+
+@receiver(post_save, sender=Post)
+def delete_unused_images(sender, instance, **kwargs): #pylint: disable=unused-argument
+    """
+    不要なメディアファイルを削除する
+    """
+    post_id = instance.id
+    body = instance.body
+
+    # この投稿の中で使用されていることを期待される画像と
+    # 使用されている記事が指定されていない画像を取得
+    used_post_images = PostImage.objects.filter(
+        Q(post=post_id) | Q(post=None)
+    )
+    for used_post_image in used_post_images:
+        # 本文中で使われているかどうかで、使用先記事を指定 or 空にする
+        url = urllib.parse.quote(str(used_post_image.image))
+        if url in body:
+            used_post_image.post = instance
+        else:
+            used_post_image.post = None
+        used_post_image.save()
+
+    # 使用されている記事が指定されていない画像を削除
+    instance.user.delete_unused_post_images()
--- posts/views.py
+++ posts/views.py
@@ -1,50 +1,17 @@
 from rest_framework import viewsets, permissions
 from rest_framework import mixins, generics
 from rest_framework.parsers import FormParser, MultiPartParser
-from django.db.models import Q
-import urllib.parse
-from .models import Post, PostImage
+from .models import Post
 from .serializers import PostSerializer, PostImageSerializer
 from .permissions import IsOwnerOrReadOnly
-
-def delete_unused_images(function):
-    """
-    不要なメディアファイルを削除する
-    """
-    def wrapper(*args, **kwargs):
-        self = args[0]
-        serializer = args[1]
-        function(*args, **kwargs)
-        body = serializer.data['body']
-        post_id = serializer.data['id']
-        post = Post.objects.get(pk=post_id)
-        # 記事内で使われることが期待される画像と所属先のない画像のオブジェクト
-        used_post_images = PostImage.objects.filter(
-            Q(post=post_id) | Q(post=None)
-        )
-        for used_post_image in used_post_images:
-            url = urllib.parse.quote(str(used_post_image.image))
-            if url in body:
-                used_post_image.post = post
-            else:
-                used_post_image.post = None
-            used_post_image.save()
-        self.request.user.delete_unused_post_images()
-    return wrapper
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
-    @delete_unused_images
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-    @delete_unused_images
-    def perform_update(self, serializer):
-        serializer.save()
-
 
 class PostImageView(mixins.CreateModelMixin, generics.GenericAPIView):
 
