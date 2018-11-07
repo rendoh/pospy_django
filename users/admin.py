0a453528271917129d11c85df8538bfdc7e75f49
Ryohei Endo <r-endo@studio-spoon.co.jp>
2018-11-07 14:56:53 +0900

    Update user models

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm, UsernameField
 from .models import User, UserAvatar, Relationship
 
 class AvatarInline(admin.StackedInline):
     model = UserAvatar
 
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email',)
+        field_classes = {'username': UsernameField}
+
 class MyUserAdmin(UserAdmin):
     inlines = (AvatarInline,)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'email')
+    form = UserChangeForm
+    add_form = MyUserCreationForm
 
 admin.site.register(User, MyUserAdmin)
 admin.site.register(UserAvatar)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,90 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.core.validators import FileExtensionValidator
+from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail
+from django.utils import timezone
 from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
-from django.core.validators import FileExtensionValidator
 
-class User(AbstractUser):
+class UserManager(BaseUserManager):
+    """ユーザーマネージャー"""
+
+    use_in_migrations = True
+
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        メールアドレスでの登録を必須にする
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(
+        _('username'),
+        max_length=30,
+    )
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        null=False,
+        blank=False,
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
     def get_followers(self):
         relations = Relationship.objects.filter(owner=self)
         return [relation.follower for relation in relations]
